886141e28e4832024520430cd1c7c503ec3b32a6
Dmitriy Uvin <dimauuvin@gmail.com>
2021-02-03 13:17:27 +0200

    Fix

--- api/database/connection/index.js
+++ api/database/connection/index.js
@@ -5,7 +5,7 @@ module.exports = new Sequelize(
     process.env.DATABASE_USERNAME,
     process.env.DATABASE_PASSWORD,
     {
-        port: proess.env.DATABASE_PORT,
+        port: process.env.DATABASE_PORT,
         host: process.env.DATABASE_HOST,
         dialect: process.env.DATABASE_DRIVER,
         logging: false,
