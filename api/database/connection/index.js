a5dfaeef7f790e8418412a4105117e4194dead0f
Dmitriy Uvin <dimauuvin@gmail.com>
2021-02-03 11:20:07 +0200

    Add port to db connection

--- api/database/connection/index.js
+++ api/database/connection/index.js
@@ -5,8 +5,9 @@ module.exports = new Sequelize(
     process.env.DATABASE_USERNAME,
     process.env.DATABASE_PASSWORD,
     {
+        port: proess.env.DATABASE_PORT,
         host: process.env.DATABASE_HOST,
         dialect: process.env.DATABASE_DRIVER,
-        logging: false
+        logging: false,
     }
 );
