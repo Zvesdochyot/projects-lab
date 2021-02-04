75083fc72f630875c9b96b70aedafa02f5cbdf97
Dmitriy Uvin <dimauuvin@gmail.com>
2021-02-04 21:37:07 +0200

    Fix

--- api/app.js
+++ api/app.js
@@ -22,6 +22,7 @@ const PORT = process.env.PORT || 3333;
 const path = require('path');
 
 app.use(express.static(path.join(__dirname, "../client/dist")));
+
 app.get("*", (req, res)=>{
     res.sendFile(path.join(__dirname, "../client/dist", 'index.html'));
 });
