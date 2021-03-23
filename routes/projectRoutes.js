ce0f8587858fd10ab72c935be7cc9dfe3f482ea3
Dmitriy Uvin <dimauuvin@gmail.com>
2021-03-23 12:32:05 +0200

    Update func

--- routes/projectRoutes.js
+++ routes/projectRoutes.js
@@ -4,5 +4,9 @@ const projectController = require('../controllers/projectController');
 
 router.get('/', projectController.getProjectsForLoggedUser);
 router.put('/change-order', projectController.reindexProjectsForLoggedUser);
+router.post('/', projectController.createProject);
+router.get('/:id', projectController.getProjectById);
+router.put('/:id', projectController.updateProject);
+router.delete('/:id', projectController.deleteById);
 
 module.exports = router;
