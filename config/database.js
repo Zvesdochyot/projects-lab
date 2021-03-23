e89e2ee5e0569db4c87169b36e97e4164fe8ad2f
Artem Sudariev <art.sudarev@gmail.com>
2021-03-24 00:31:49 +0200

    explanatory-note-v1

--- config/database.js
+++ config/database.js
@@ -1,8 +1,8 @@
 module.exports = {
     development: {
-        username: 'admin',
-        password: 'password',
-        database: 'projects-lab-dev',
+        username: 'root',
+        password: 'Claster007ua',
+        database: 'projects_lab',
         host: 'localhost',
         dialect: 'mysql'
     },
