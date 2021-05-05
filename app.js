e2b13a1f4665baf89e53c6c009097ad51e672859
Rostyslav Nakarlovich <zvesdochyot@gmail.com>
2021-05-05 21:05:43 +0300

    Initial Swagger setup

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const express = require('express');
 const app = express();
 const dbConnection = require('./database/connection');
 const cors = require('cors');
+const swaggerJSDoc = require('swagger-jsdoc');
+const swaggerUI = require('swagger-ui-express');
 const passportSetup = require('./config/passport');
 const errorHandler = require('./middlewares/errorHandler');
 
@@ -12,9 +14,29 @@ const corsOptions = {
     origin: process.env.CLIENT_APP_URL
 };
 
+const swaggerOptions = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'ProjectsLab',
+            version: '1.0.0',
+            description: 'ProjectsLab Library API'
+        },
+        servers: [
+            {
+                url: 'http://localhost:3333'
+            }
+        ]
+    },
+    apis: ['../routes/*.js']
+};
+
+const swaggerSpecs = swaggerJSDoc(swaggerOptions);
+
 app.use(cors(corsOptions));
 app.use(express.json());
 
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerSpecs));
 app.use('/api/v1', require('./routes'));
 app.use(errorHandler);
 
