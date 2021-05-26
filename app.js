821adc3f1bec5098b70850d28a89dd4e32568d10
Artem Sudariev <art.sudarev@gmail.com>
2021-05-26 23:59:41 +0300

    Sequence dia's

--- app.js
+++ app.js
@@ -52,6 +52,8 @@ app.use(express.static(path.join(__dirname, '../client/dist')));
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'client/dist', 'index.html'));
 
+});
+
 dbConnection.authenticate()
     .then(() => {
         console.log('MySQL connected!');
--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 module.exports = {
     development: {
         username: 'root',
-        password: 'projectslab',
+        password: 'Claster007ua',
         database: 'projects_lab',
         host: 'localhost',
         dialect: 'mysql'
