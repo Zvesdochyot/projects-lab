d6dc732ff44f4155d86035d13e22262bdb916c06
Rostyslav Nakarlovich <zvesdochyot@gmail.com>
2021-05-26 23:13:58 +0300

    Quick fixes here and there

--- client/src/views/Project.vue
+++ client/src/views/Project.vue
@@ -39,8 +39,7 @@
                                     <v-card-title class="headline">
                                         Use Google's location service?
                                     </v-card-title>
-                                    <v-card-text>Let Google help apps determine location. This means sending anonymous
-                                        location data to Google, even when no apps are running.
+                                    <v-card-text>Let Google help apps determine location. This means sending anonymous location data to Google, even when no apps are running.</v-card-text>
                                     <v-card-actions>
                                         <v-spacer></v-spacer>
                                         <VBtn
